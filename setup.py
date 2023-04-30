82d8c86a43932718be242cd389eb5d1b2f6cbd17
Fabian Braun <fsbraun@gmx.de>
2023-04-30 18:28:12 +0200

    Dark mode

---
+++ setup.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+from djangocms_simple_admin_style import __version__
+
+
+REQUIREMENTS = [
+    "django-cms",
+]
+
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Framework :: Django',
+    'Framework :: Django :: 2.2',
+    'Framework :: Django :: 3.1',
+    'Framework :: Django :: 3.2',
+    'Framework :: Django :: 4.0',
+    'Framework :: Django :: 4.1',
+    'Framework :: Django :: 4.2',
+    'Framework :: Django CMS',
+    'Framework :: Django CMS :: 3.6',
+    'Framework :: Django CMS :: 3.7',
+    'Framework :: Django CMS :: 3.8',
+    'Framework :: Django CMS :: 3.8',
+    'Framework :: Django CMS :: 3.9',
+    'Framework :: Django CMS :: 3.10',
+    'Framework :: Django CMS :: 3.11',
+    'Framework :: Django CMS :: 4.0',
+    'Framework :: Django CMS :: 4.1',
+    'Framework :: Django CMS :: 4.2',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries',
+]
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.rst").read_text()
+
+setup(
+    name='djangocms-simple-admin-style',
+    version=__version__,
+    author='Fabian Braun',
+    author_email='fsbraun@gmx.de',
+    maintainer='Django CMS Association and contributors',
+    maintainer_email='info@django-cms.org',
+    url='https://github.com/fsbraun/djangocms-simple-admin-style',
+    license='BSD-3-Clause',
+    description='Adds pretty CSS styles for the django CMS admin interface.',
+    long_description=long_description,
+    packages=find_packages(exclude=['preview', 'tests']),
+    python_requires='>=3.7',
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=REQUIREMENTS,
+    classifiers=CLASSIFIERS,
+    test_suite='tests.settings.run',
+)
